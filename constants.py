5a66491d73595a3a140f5ebd236ce6b5d8ca06bd
DinoWithCurls <adityarajsingh64@gmail.com>
2022-09-05 17:47:11 +0530

    fix: 🐛 Created a constants file, added comments

---
+++ constants.py
@@ -0,0 +1,14 @@
+#The constants are to be stored in this file. 
+BLACK = 0, 0, 0
+WHITE = 255, 255, 255
+GREEN = 0, 255, 0
+BLUE = 127, 0, 255
+
+BAR_GRADIENTS = [
+		(153, 255, 153),
+		(255, 102, 178),
+		(192, 192, 192)
+	]
+
+HORIZONTAL_PADDING = 100
+TOP_PADDING = 100
--- visualiser.py
+++ visualiser.py
@@ -1,27 +1,17 @@
 import pygame
 import random
 import math
+
+import constants
+
 pygame.init()
 
 
 class View:
-	black = 0, 0, 0
-	white = 255, 255, 255
-	green = 0, 255, 0
-	blue = 127, 0, 255
-	bg_color = white
-
-	gradient = [
-		(153, 255, 153),
-		(255, 102, 178),
-		(192, 192, 192)
-	]
+	bg_color = constants.WHITE
 
 	normal_font = pygame.font.SysFont('helvetica', 24)
 	title_font = pygame.font.SysFont('helvetica', 40)
-
-	horizontal_padding = 100
-	top_padding = 100
 
 	def __init__(self, width, height, lst):
 		self.width = width
@@ -36,40 +26,40 @@ class View:
 		self.min_item = min(lst)
 		self.max_item = max(lst)
 
-		self.block_width = round((self.width - self.horizontal_padding) / len(lst))
+		self.block_width = round((self.width - constants.HORIZONTAL_PADDING) / len(lst))
 		self.block_height = math.floor(
-		    (self.height - self.top_padding) / (self.max_item - self.min_item))
-		self.start_x = self.horizontal_padding // 2
+		    (self.height - constants.TOP_PADDING) / (self.max_item - self.min_item))
+		self.start_x = constants.HORIZONTAL_PADDING // 2
 
-
+#This function is for the whole view
 def draw(view, algo, ascending):
 	view.window.fill(view.bg_color)
 
 	title = view.title_font.render(
-	    f"{algo} - {'Ascending' if ascending else 'Descending'}", 1, view.black)
+	    f"{algo} - {'Ascending' if ascending else 'Descending'}", 1, constants.BLACK)
 	view.window.blit(title, (view.width/2 - title.get_width()/2, 5))
 
 	controls = view.normal_font.render(
-	    "R - Reset | SPACE - Start Sorting | A - Ascending | D - Descending", 1, view.black)
+	    "R - Reset | SPACE - Start Sorting | A - Ascending | D - Descending", 1, constants.BLACK)
 	view.window.blit(controls, (view.width/2 - controls.get_width()/2, 45))
 
 	draw_list(view)
 	pygame.display.update()
 
-
+#This function is for the sorting visualisation
 def draw_list(view, color_positions={}, clear_bg=False):
 	lst = view.lst
 
 	if clear_bg:
-		clear_rect = (view.horizontal_padding//2, view.top_padding,
-						view.width - view.horizontal_padding, view.height - view.top_padding)
+		clear_rect = (constants.HORIZONTAL_PADDING//2, constants.TOP_PADDING,
+						view.width - constants.HORIZONTAL_PADDING, view.height - constants.TOP_PADDING)
 		pygame.draw.rect(view.window, view.bg_color, clear_rect)
 
 	for i, val in enumerate(lst):
 		x = view.start_x + i * view.block_width
 		y = view.height - (val - view.min_item) * view.block_height
 
-		color = view.gradient[i % 3]
+		color = constants.BAR_GRADIENTS[i % 3]
 
 		if i in color_positions:
 			color = color_positions[i]
@@ -79,7 +69,7 @@ def draw_list(view, color_positions={}, clear_bg=False):
 	if clear_bg:
 		pygame.display.update()
 
-
+# For generating a random array.
 def generate_array(n, min_item, max_item):
 	lst = []
 	for _ in range(n):
@@ -87,7 +77,7 @@ def generate_array(n, min_item, max_item):
 		lst.append(val)
 	return lst
 
-
+#This is temporary, just for testing the visualiser. Will be removed when other algorithms are added.
 def bubble_sort(view, ascending=True):
 	lst = view.lst
 	for i in range(len(lst) - 1):
@@ -97,7 +87,7 @@ def bubble_sort(view, ascending=True):
 
 			if (num1 > num2 and ascending) or (num1 < num2 and not ascending):
 				lst[j], lst[j + 1] = lst[j + 1], lst[j]
-				draw_list(view, {j: view.green, j + 1: view.blue}, True)
+				draw_list(view, {j: constants.GREEN, j + 1: constants.BLUE}, True)
 				yield True
 
 	return lst
@@ -137,17 +127,17 @@ def main():
 				continue
 
 			if event.key == pygame.K_r:
-				lst = generate_array(n, min_item, max_item)
+				lst = generate_array(n, min_item, max_item)			#The array to be sorted is generated
 				view.set_list(lst)
 				sorting = False
-			elif event.key == pygame.K_SPACE and sorting == False:
+			elif event.key == pygame.K_SPACE and sorting == False:	#When the spacebar is pressed on the keyboard, the array is sorted and this is visualised on the screen
 				sorting = True
 				sorting_algorithm_generator = sorting_algorithm(view, ascending)
-			elif event.key == pygame.K_a and not sorting:
+			elif event.key == pygame.K_a and not sorting:			#When A is pressed on the keyboard
 				ascending = True
-			elif event.key == pygame.K_d and not sorting:
+			elif event.key == pygame.K_d and not sorting:			#When D is pressed on the keyboard
 				ascending = False
-			elif event.key == pygame.K_b and not sorting:
+			elif event.key == pygame.K_b and not sorting:			#When B is pressed on the keyboard
 				sorting_algorithm = bubble_sort
 				sorting_algo_name = "Bubble Sort"
 	pygame.quit()
