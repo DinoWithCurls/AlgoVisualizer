8e91fa47e4c7535d9493fe38512de23ef5e906be
DinoWithCurls <adityarajsingh64@gmail.com>
2022-09-07 21:18:28 +0530

    test: 💍 Added unit tests for bubble sort

--- algos/bubbleSort.py
+++ algos/bubbleSort.py
@@ -1,10 +1,18 @@
-def bubbleSort(arr, *args):
-    for i in range(0, len(arr)):
-        swap = False
-        for j in range(len(arr)-i-1):
-            yield arr, j, j+1, -1, -1
-            if(arr[j] > arr[j+1]):
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                swap = True
-        if not swap:
-            break
+import sys
+
+sys.path.append("..")
+
+from visualiser import draw_list
+
+def bubbleSort(arr, ascending=True):
+    #array = view.lst
+    array = arr
+    for i in range(len(array) - 1):
+        for j in range(len(array) - 1 - i):
+            num1 = array[j]
+            num2 = array[j + 1]
+            if (num1 > num2 and ascending) or (num1 < num2 and not ascending):
+                array[j], array[j + 1] = array[j + 1], array[j]
+                #draw_list(view, {j: constants.GREEN, j + 1: constants.BLUE}, True)
+                #yield True
+    return array
---
+++ algos/unitTests.py
@@ -0,0 +1,42 @@
+import unittest
+
+#import mergeSort
+from bubbleSort import bubbleSort
+#import insertionSort
+#import quickSort
+#import countSort
+#import radixSort
+#import heapSort
+
+unsorted_positive_array = [5, 5, 7, 8, 2, 4, 1]
+
+unsorted_negative_array = [-1, -3, -5, -7, -9, -5]
+
+unsorted_integer_array = [-6, 0, -4, -5, 5, 5, 7, 8, 2, 4, 1]
+
+empty_array = []
+
+duplicate_number_array = [2, 2, 2, 2]
+
+error_message = "The expected and achieved outcomes are not same"
+
+class UnitTest(unittest.TestCase):
+    def _test_sort(self, sort_func, input_list):
+        expected_list = sorted(input_list)
+        self.assertEqual(sort_func(input_list), expected_list, error_message)
+    def test_bubble_sort(self):
+        print("Testing Bubble Sort")
+        print("For positive numbers")
+        self._test_sort(bubbleSort, unsorted_positive_array)
+        print("For negative numbers")
+        self._test_sort(bubbleSort, unsorted_negative_array)
+        print("For both positive and negative numvers")
+        self._test_sort(bubbleSort, unsorted_integer_array)
+        print("For array with same numbers")
+        self._test_sort(bubbleSort, duplicate_number_array)
+        print("For empty array")
+        self._test_sort(bubbleSort, unsorted_positive_array)
+
+if __name__ == '__main__':
+    unittest.main()
+    
