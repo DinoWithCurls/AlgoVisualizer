71fb1e3e0d6c8d61e10835cdd57f8eda0f830fca
DinoWithCurls <adityarajsingh64@gmail.com>
2022-09-19 16:49:12 +0530

    feat: 🎸 Visualiser added for selection sort

--- algos/selectionSort.py
+++ algos/selectionSort.py
@@ -3,16 +3,17 @@ sys.path.append("..")
 import constants
 from visualiser import draw_list
 
-def selection_sort(array, ascending= True):
+def selection_sort(view, ascending= True):
+    array = view.array
     size = len(array)
     for step in range(size):
         min_idx = step
         for i in range(step + 1, size):
-            # to sort in descending order, change > to < in this line
-            # select the minimum element in each loop
-            if array[i] < array[min_idx]:
+            if (array[i] < array[min_idx] and ascending) or (array[i] > array[min_idx] and not ascending):
                 min_idx = i
-         
-        # put min at the correct position
+                draw_list(view, {i: constants.BLACK, min_idx: constants.BLUE}, True)
+                yield True
         (array[step], array[min_idx]) = (array[min_idx], array[step])
+        draw_list(view, {step: constants.GREEN, min_idx: constants.BLUE}, True)
+        yield True
     return array
